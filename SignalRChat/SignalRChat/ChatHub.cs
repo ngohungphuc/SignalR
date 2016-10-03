cd2c696ed1874cec35682ffeea015f9899957d54
ngohungphuc <ngohungphuc95@gmail.com>
2016-10-03 11:14:35 +0700

    SignalR server setup

---
+++ SignalRChat/SignalRChat/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
