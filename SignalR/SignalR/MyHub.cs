170cba80707a5eaeff8abb407d106431279f8a5b
ngohungphuc <ngohungphuc95@gmail.com>
2017-01-16 07:49:28 +0700

    Connect to the hub

---
+++ SignalR/SignalR/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR
+{
+    [HubName("myhub")]
+    public class MyHub : Hub
+    {
+    }
+}
