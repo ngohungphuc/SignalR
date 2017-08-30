2a65abe23dddcbae91867a4222da7c91011fe6be
ngohungphuc <ngohungphuc95@gmail.com>
2017-08-30 22:34:15 +0700

    signal core

---
+++ SignalRCore/SignalRCore/Hubs/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCore.Hubs
+{
+    public class MyHub : Hub
+    {
+        public void MyServerFunction()
+        {
+            Clients.All.clientListener($"Your connection id is: {Context.ConnectionId}");
+        }
+    }
+}
