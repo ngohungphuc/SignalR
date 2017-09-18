600361815326fdc040e56add49099dac1e1cc948
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-18 11:25:55 +0700

    add signal core 2.0 sample

---
+++ Samples/SignalR-samples/StockTickR/StockTickRApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalR.StockTicker
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+
+            services.AddScoped<StockTickerHub>();
+            services.AddSingleton<StockTicker>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseFileServer();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<StockTickerHub>("stocks");
+            });
+        }
+    }
+}
